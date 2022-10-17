247a440d9cdd776e569707ed3c87436cc2289cea
rebeccathomas-NOAA <79660143+rebeccathomas-NOAA@users.noreply.github.com>
2022-10-17 14:35:34 -0700

    Updated to using documentation with Roxygen.

--- R/repath_EVdir.R
+++ R/repath_EVdir.R
@@ -1,58 +1,68 @@
+#' repath_EVdir.R
+#'
+#' @description Function to take a directory of EV files and reset the path.
+#' Written by RT 2022.
+#' Requires an excel file input hat says what the paths are
+#' Each row is a survey (survey_name), and each column is a path or file name
+#'
+#' @param SurveyName name of survey as in excel file
+#' @param DirNameFile excel file name of paths
+#' @param ni is an optional argument for which file to start with (default ni=1 is the first file).
+#'
+#' Excel file has the following columns (with header names)
+#' Cal_File: calibration file name
+#' Base_Path:
+#' Orig_EV_Dir:
+#' Raw_Dir
+#' EK80_Cal_File
+#' EK80_Raw_Dir
+#'
+#' @examples
+#' DirNameFile="C:/rthomas/R/Rcode/EK60_EK80 conversion/EK60_Ek80_conv/Directory Structure EK60 EK80 conversion.xlsx"
+#' SurveyName='2019_US'
+#' repath_EVdir(SurveyName,DirNameFile)
+#'
+#' @export
 repath_EVdir <- function(Survey_Name, DirNameFile, ni=1,...) {
 
-### repath_EVdir.R
-# Script to take a directory of EV files and reset the path
-# Written by RT 2022
-# Requires an excel file (put as input?) that says what the paths are
-# Each row is a survey (survey_name?), and each column is a path or file name
-# Inputs: SurveyName, excel file name of paths, ni is an optional argument for which 
-# file to start with (default ni=1 is the first file).
-###
-# Excel file:
-# Cal_File: calibration file name
-# Base_Path:  
-# Orig_EV_Dir: 
-# Raw_Dir
-# EK80_Cal_File
-# EK80_Raw_Dir
 ###
 
 # Rename as for EK60/EK80 or figure out how to make it work with both (keeping in mind may sometimes have other filesets).
 
   #require(RDCOMClient)
   #require(readxl)
-  
+
   #DirNameFile="C:/rthomas/R/Rcode/EK60_EK80 conversion/EK60_Ek80_conv_updated/Directory Structure EK60 EK80 conversion updated.xlsx"
   #SurveyName='2019_US'
-  
+
   ###############################################
   #           Set Paths                         #
   ###############################################
-  
+
   DirTable <- readxl::read_excel(DirNameFile)
   DirTableSurvey<- subset(DirTable, Survey == SurveyName)
   DirTableSurvey[] <- lapply(DirTableSurvey, function(x) if(is.factor(x)) factor(x) else x)  #to ensure dataframe actually removed other variables in subset
-  BasePath<-DirTableSurvey$Base_Path 
+  BasePath<-DirTableSurvey$Base_Path
   BaseJudgePath<-DirTableSurvey$Orig_EV_Dir
-  
+
   #calibration
   CALfileName<-DirTableSurvey$Cal_File
   CALfileName_EK80<-DirTableSurvey$EK80_Cal_File
-  
+
   #location of .raw files
   RAWdir<-DirTableSurvey$Raw_Dir
   RAWdir_EK80<-DirTableSurvey$EK80_Raw_Dir
-  
+
   #location of transect EV files
   Transectdir<-DirTableSurvey$Orig_EV_Dir
-  
+
   ###############################################
   #         Update paths in Echoview            #
   ###############################################
-  
+
   # list the EV files
   EVfile.list <- list.files(file.path(Transectdir), pattern="*.EV")
-  
+
   # create COM connection between R and Echoview
   EVApp <- RDCOMClient::COMCreate("EchoviewCom.EvApplication")
   nfiles=length(EVfile.list)  #number of EV files
@@ -60,20 +70,20 @@ repath_EVdir <- function(Survey_Name, DirNameFile, ni=1,...) {
   ## ------------- start loop
   for (i in EVfile.list[ni:nfiles]){
     ni=which(EVfile.list==i)
-    print(ni)   
+    print(ni)
     EVfileName <- file.path(Transectdir, i)
     print(EVfileName)
-    
+
     # open EV file
     EVfile <- EVApp$OpenFile(EVfileName)
-    
-    #set the first fileset object 
+
+    #set the first fileset object
     filesetObj <- EVfile[["Filesets"]]$Item(0)  #RAW
-    
+
     #######
     # EK60
     #######
-    
+
     # set data properties object and input new datafiles folder location
     evPropObj <- EVfile[["Properties"]]
     evPropObj[["DataPaths"]]$Clear()
@@ -82,13 +92,13 @@ repath_EVdir <- function(Survey_Name, DirNameFile, ni=1,...) {
     # set calibration file
     add.calibration <- filesetObj$SetCalibrationFile(CALfileName)
     print(paste("calibration added: ",add.calibration))
-    
+
     #######
     # EK80
     #######
     nfilesets=EVfile[["Filesets"]]$Count()  #number of filesets
     if (nfilesets>1) {
-    #set the first fileset object 
+    #set the first fileset object
     filesetObj <- EVfile[["Filesets"]]$Item(1)  #RAW
     # set data properties object and input new datafiles folder location
     evPropObj <- EVfile[["Properties"]]
@@ -98,15 +108,15 @@ repath_EVdir <- function(Survey_Name, DirNameFile, ni=1,...) {
     add.calibration <- filesetObj$SetCalibrationFile(CALfileName_EK80)
     print(paste("calibration added: ",add.calibration))
     }
-    
+
     # save EV file
     EVfile$Save()
-    
+
     #close EV file
     EVApp$CloseFile(EVfile)
-  
+
   }
   ##########################################
-  
+
   EVApp$Quit()  #quit echoview
 }
