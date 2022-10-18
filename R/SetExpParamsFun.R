26c5420c51bd48c2719dfc535d558a7b0c45c14e
rebeccathomas-NOAA <79660143+rebeccathomas-NOAA@users.noreply.github.com>
2022-10-18 14:38:13 -0700

    batch push of update - copy from working folder

---
+++ R/SetExpParamsFun.R
@@ -0,0 +1,24 @@
+#'SetExpParamsFun(Obj_ExpVar, ToExport,NoExport)
+#'
+#' @description Function to take an EvFile Export object and set the export parameters within it. 
+#' Parameters to export come in as ToExport. Parameters not to export come in as NoExport
+#' 
+#' @param Obj_ExpVar- an Evfile export object of the form EVfile[['Properties']][['Export']][['Variables']]
+#' 
+#' @example SetBiomassExpParamsFun(Obj_ExpVar, ToExport,NoExport)
+#' 
+#' @export
+SetExpParamsFun<-function(Obj_ExpVar, ToExport,NoExport){
+
+     nToExport = length(ToExport)
+     nNoExport = length(NoExport)
+     for (k in seq_len(nToExport)){
+          Obj<-Obj_ExpVar$Item(ToExport[k])
+          Obj[["Enabled"]]<-TRUE
+     }
+     for (k in seq_len(nNoExport)){
+          Obj<-Obj_ExpVar$Item(NoExport[k])
+          Obj[["Enabled"]]<-FALSE
+     }
+}
+
