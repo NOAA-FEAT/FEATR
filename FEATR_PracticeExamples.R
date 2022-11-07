964ab6d63b9e662171eb024386a90a40a78e1105
Elizabeth Phillips <elizabeth.phillips@noaa.gov>
2022-11-07 15:27:40 -0800

    Add script to practice with functions

---
+++ FEATR_PracticeExamples.R
@@ -0,0 +1,6 @@
+#devtools::install_github("NOAA-FEAT/FEATR")
+library(FEATR)
+#dirstruct <- read_excel("E:/GitHub/FEATR/R/Directory Structure EK60 EK80 conversion updated.xlsx")
+DirNameFile <- "E:/GitHub/FEATR/R/Directory Structure EK60 EK80 conversion updated.xlsx"
+SurveyName <- "2019_US"
+repath_EVdir(SurveyName,DirNameFile, ni=1)
