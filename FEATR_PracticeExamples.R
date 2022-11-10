7d26fb9368bd07455e79e199d08e18cd2b5a94b6
Elizabeth Phillips <elizabeth.phillips@noaa.gov>
2022-11-10 14:24:28 -0800

    Update FEATR_PracticeExamples.R

--- FEATR_PracticeExamples.R
+++ FEATR_PracticeExamples.R
@@ -4,3 +4,5 @@ library(FEATR)
 DirNameFile <- "E:/GitHub/FEATR/R/Directory Structure EK60 EK80 conversion updated.xlsx"
 SurveyName <- "2019_US"
 repath_EVdir(SurveyName,DirNameFile, ni=1)
+
+SetBiomassExpParamsFun(Obj_ExpVar, ToExport,NoExport)
